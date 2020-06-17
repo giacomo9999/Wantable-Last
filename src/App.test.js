a10fac2dccb3a743532c9f5e9e80c53bd66f42a9
giacomo9999 <gmarchesi888@gmail.com>
2020-06-17 16:49:09 -0400

    Added redux and react-redux dependencies.

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,7 @@ import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
-
 Enzyme.configure({ adapter: new EnzymeAdapter() });
-
-
 
 test("renders without error", () => {
   const wrapper = shallow(<App />);
